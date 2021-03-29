06ff397cce427ce9ac284625b6fc937e56b07a50
Kyle Young <kyleyoung.9210@gmail.com>
2021-03-29 18:30:38 +0000

    refactoring test syntax

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,25 +1,24 @@
 const Employee = require("../lib/Employee");
+const employee = new Employee("Zoe", 1, "zoe@email.com");
 
 describe("Employee", () => {});
 describe("Employee name", () => {
   it("Returns the employee's name when a new employee is added", () => {
-    expect(new Employee("Zoe").getName()).toBe("Zoe");
+    expect(employee.getName()).toBe("Zoe");
   });
 });
 describe("Employee ID", () => {
   it("Returns the Employee ID when a new employee is added", () => {
-    expect(new Employee("Zoe", 1).getID()).toBe(1);
+    expect(employee.getID()).toBe(1);
   });
 });
 describe("Employee email", () => {
   it("Returns the employee's email when a new employee is added", () => {
-    expect(new Employee("Zoe", 1, "zoe@email.com").getEmail()).toBe(
-      "zoe@email.com"
-    );
+    expect(employee.getEmail()).toBe("zoe@email.com");
   });
 });
 describe("Employee role", () => {
   it('Returns the role of the new employee as "Employee"', () => {
-    expect(new Employee("Zoe", 1, "zoe@email.com").getRole()).toBe("Employee");
+    expect(employee.getRole()).toBe("Employee");
   });
 });
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,14 +1,13 @@
 const Manager = require("../lib/Manager");
+const manager = new Manager("Zoe", 1, "zoe@email.com", 1);
 
-describe("Manager office number", () => {
+describe("Manager's office number", () => {
   it("Returns the Manager's office number", () => {
-    const manager = new Manager("Zoe", 1, "zoe@email.com", 1).getOfficeNumber();
-    expect(manager).toBe(1);
+    expect(manager.getOfficeNumber()).toBe(1);
   });
 });
 describe("Manager role", () => {
   it('Returns the role of the new employee as "Manager"', () => {
-    const manager = new Manager("Zoe", 1, "zoe@email.com", 1).getRole();
-    expect(manager).toBe("Manager");
+    expect(manager.getRole()).toBe("Manager");
   });
 });
